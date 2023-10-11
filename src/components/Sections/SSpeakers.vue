3415665717c9e6048102b7056a0551c992b6f882
Francesc Arpí Roca <francescarpi@users.noreply.github.com>
2023-10-11 09:13:12 +0100

    feat(speakers): add first iteration of speakers (#16)

--- src/components/Sections/SSpeakers.vue
+++ src/components/Sections/SSpeakers.vue
@@ -9,13 +9,17 @@ import ALink from '../Atoms/ALink.vue'
 <template>
   <a-section>
     <a-title>Ponents</a-title>
-    <div >
+    <div class="md:grid md:grid-cols-2 md:gap-4">
       <a-box v-for="speaker in speakers" :key="speaker.name" class="mb-4">
         <h4 class="font-bold">"<i>{{ speaker.talk }}</i>" per {{ speaker.name }}</h4>
-        <a-link v-if="speaker.twitter" target="_blank" :href="`https://twitter.com/${speaker.twitter}`" class="text-sm">@{{ speaker.twitter }}</a-link>
-        <a-link v-if="speaker.linkedin" target="_blank" :href="`https://www.linkedin.com/in//${speaker.linkedin}`" class="text-sm">@{{ speaker.linkedin }}</a-link>
-        <p :key="summaryCount" v-for="(summaryEntry, summaryCount) in speaker.summary" id="summary" class="mt-4 text-justify" v-html="summaryEntry"/>
-        <p :key="bioCount" v-for="(bioEntry, bioCount) in speaker.bio" id="bio" class="mt-4 text-justify" v-html="bioEntry"/>
+        <a-link v-if="speaker.twitter" target="_blank" :href="`https://twitter.com/${speaker.twitter}`"
+          class="text-sm">@{{ speaker.twitter }}</a-link>
+        <a-link v-if="speaker.linkedin" target="_blank" :href="`https://www.linkedin.com/in//${speaker.linkedin}`"
+          class="text-sm">@{{ speaker.linkedin }}</a-link>
+        <p :key="summaryCount" v-for="(summaryEntry, summaryCount) in speaker.summary" id="summary"
+          class="mt-4 text-justify" v-html="summaryEntry" />
+        <p :key="bioCount" v-for="(bioEntry, bioCount) in speaker.bio" id="bio" class="mt-4 text-justify"
+          v-html="bioEntry" />
       </a-box>
     </div>
   </a-section>
--- src/content.ts
+++ src/content.ts
@@ -103,74 +103,44 @@ export const diary: IEvent[] = [
 
 export const speakers: ISpeaker[] = [
   {
-    name: 'Gessi Paniagua',
-    twitter: 'gessipani',
-    talk: 'Taller introducció a Django',
+    name: 'Laura Mora i Aubert',
+    twitter: 'blackhold_',
+    talk: 'Connectat la teva aplicació amb la IA',
     summary: [
-      'Vols aprendre a programar i crear les teves pròpies aplicacions web?',
-      "Durant el taller aprendrem com emprar el <a class='text-third' href='https://www.djangoproject.com/' target='_blank'>framework web Django</a> seguint la filosofia Django Girls: petits grups liderats per una mentora que segueixen el tutorial d'iniciació a Django."
-    ],
-    bio: [
-      'Miembro de PyLadies España, Python España y co-fundadora de LinuxChixAr, actualmente trabaja en Mática Partners como Data Engineer.'
-    ]
-  },
-  {
-    name: 'Pau Boix',
-    talk: "Control de l'automatització de tasques recurrents emprant Airflow",
-    summary: [
-      '<a class="text-third" target="_blank" href="https://airflow.apache.org/">Apache Airflow</a> és una plataforma open source de gestió de fluxes de treball.',
-      "A la presentació veurem els conceptes bàsics d'Airflow i com podem desenvolupar el nostre primer DAG."
-    ],
-    bio: [
-      "En Pau s'acaba de graduar com a enginyer infromàtic per la UdG i porta més d'un any treballant a Som Energia on ha realitzat el seu TFG centrat en l'implantació d'Apache Airflow a la cooperativa."
-    ]
-  },
-  {
-    name: 'Pablo Ridolfi',
-    twitter: 'pabloridolfi',
-    talk: 'ESP32 emprant C + SSL',
-    summary: [
-      'Obtén el máximo rendimiento de tu ESP32: desarrolla aplicaciones en lenguaje C con el <a class="text-third" target="_blank" href="https://docs.espressif.com/projects/esp-idf/en/latest/esp32/">framework ESP-IDF</a>.',
-      'La familia de microcontroladores ESP32 se ha vuelto un estándar de facto para el desarrollo de hardware IoT gracias a sus prestaciones de conectividad y su compatibilidad con plataformas como Arduino y Micropython.',
-      'En esta presentación abordaremos aspectos básicos del Espressif IoT Development Framework (ESP-IDF) que nos permitirá desarrollar aplicaciones en lenguaje C buscando obtener el mayor rendimiento de esta plataforma de hardware. Finalmente realizaremos un ejercicio de conectividad SSL desde un dispositivo basado en ESP32 hacia un servidor en la nube.'
-    ],
-    bio: [
-      'Pablo es ingeniero en electrónica (Universidad Tecnológica Nacional) y magíster en sistemas embebidos (Universidad de Buenos Aires). Actualmente es lead field application engineer en Satellogic, donde es responsable por maximizar las métricas de rendimiento, confiabilidad y calidad de la flota de satélites LEO de la compañía.'
-    ]
-  },
-  {
-    name: 'Oriol Piera',
-    twitter: 'cortsenc',
-    talk: "Introducció a Odoo, l'ERP de Python",
-    summary: [
-      "Durant la xerrada es presentarà <a class='text-third' target='blank' href='https://www.odoo.com/'>Odoo</a> i veurem com podem fer el nostre primer mòdul."
+      "Dingo és una aplicació desenvolupada amb Django que es connecta a varis chatbots entrenats amb procediments d'actuació i diferents prompts per el tipus d'interlocutor que interactua amb el programa. Si l'interlocutor està autenticat al sistema podrà fer la seva consulta per veu o per text, el chatbot respon i aleshores aquesta resposta serà possible traduir-la i reproduir-la a més d'una trentena d'idiomes."
     ],
     bio: []
   },
   {
-    name: 'Jorge Salazar',
-    linkedin: 'jorgesalazarm',
-    talk: 'OS datawarehouse',
+    name: 'Óscar Tienda Beteta',
+    twitter: '',
+    talk: 'Me suenas: cómo nos conocimos antes de conocernos',
     summary: [
-      'How to create a highly operational Data Warehouse with Open-Source tools through a layered architecture that allows responding to the business needs that Data Scientists and business intelligence specialists solve.',
-      'Possible Big Data and Data Lake strategies. Proposal for the development cycle of solutions with Machine Learning.'
+      'En la charla "Me suenas: cómo nos conocimos antes de conocernos", se presenta un proyecto personal en el que se utilizan datos geográficos de Google para descubrir cuándo me crucé con mi pareja antes de conocernos formalmente. El proyecto, desarrollado en Python con herramientas open source de análisis de datos, se ofrece como un caso práctico para entender el alcance y limitaciones de este tipo de datos geográficos.'
     ],
-    bio: [
-      'Computer engineer Jorge Salazar Marrero with more than 13 years of experience in Data environments. I worked as Team Lead of a team of Data Analysts at Hewlett Packard Enterprise and I currently work as Team Lead of the Kave Home S.L. Data Team.'
-    ]
+    bio: []
   },
   {
-    name: 'Israel Saeta',
-    // linkedin: 'dukebody',
-    twitter: 'dukebody',
-    talk: 'Crea la teva web amb un Static Site Generator',
+    name: 'Victor Carreras',
+    twitter: 'vcarreras9',
+    talk: 'Flutter, la nueva era de aplicaciones móviles',
     summary: [
-      "En els últims anys s'han anat popularitzant els Static Site Generators (SSG) com a una alternativa senzilla, eficient i segura per crear pàgines webs.",
-      'En aquesta xerrada explicarem els principis bàsics i mostrarem exemples en alguns dels sistemes de SSG més populars.'
+      '¿Has oído hablar de Flutter? Aprende a hacer tu primera aplicación en Flutter y veremos todo lo que hay bajo el capó de esta maravillosa tecnología de Google.'
     ],
-    bio: [
-      'Apassionat de la informàtica des de petitet. Vaig aprendre a programar amb recursos gratuïts i llibres. Actualment treballo a TravelPerk dirigint un equip i programant en Python i una mica en JavaScript quan tinc temps :)',
-      'També he col·laborat en múltiples comunitats relacionades amb Python com PyBCN i Python España.'
-    ]
+    bio: []
+  },
+  {
+    name: 'Carlos Afonso',
+    twitter: '',
+    talk: 'Introducción a Kubernetes.',
+    summary: [''],
+    bio: []
+  },
+  {
+    name: 'Joan Leon',
+    twitter: 'nucliweb',
+    talk: 'Detectant problemes de web performance amb DevTools.',
+    summary: [''],
+    bio: []
   }
 ]
